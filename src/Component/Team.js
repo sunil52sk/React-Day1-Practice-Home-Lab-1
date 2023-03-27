94afbfb4ccbd100c0e532dbd675767a7be4bbc05
sunilkumar_seenivasan <100457242+sunil52sk@users.noreply.github.com>
2023-03-27 11:08:35 +0530

    Add files via upload

---
+++ src/Component/Team.js
@@ -0,0 +1,13 @@
+import React from 'react'
+function Team() {
+  return (
+    <div>This is Team Functional Component</div>
+  )
+  }
+function World() {
+  return (
+    <div>This is World Functional Component</div>
+  )
+}
+
+export default World
